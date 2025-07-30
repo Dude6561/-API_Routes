e9be238c07bd5e09f9e3f6320a5f5b7820abc76f
DUDE <nisbis194@gmail.com>
2025-07-30 10:13:51 +0545

    LEarnt about token request and headers

---
+++ api_demo/src/app/profile/route.ts
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import { NextRequest } from "next/server";
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
+  return new Response(" THis is profile Api", {
+    headers: { "Content-Type": "text/html" },
+  });
+}
