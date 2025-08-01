03ee483e9a51309b480631bbf421b8a12459ca32
DUDE <nisbis194@gmail.com>
2025-08-01 22:17:11 +0545

    Learlnt to use cookies

--- api_demo/src/app/profile/route.ts
+++ api_demo/src/app/profile/route.ts
@@ -1,9 +1,12 @@
+import { log } from "console";
 import Head from "next/head";
 import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   console.log(requestHeaders.get("Authorization"));
+  const theme = request.cookies.get("Theme");
+  console.log(theme);
   return new Response(" THis is profile Api", {
-    headers: { "Content-Type": "text/" },
+    headers: { "Content-Type": "text/html", "Set-Cookie": "Theme=Dark" },
   });
 }
