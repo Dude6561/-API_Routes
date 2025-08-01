9ad3c6ecf5723c8dd410fae60d9d7ff7ec7e3f8e
DUDE <nisbis194@gmail.com>
2025-08-01 22:26:29 +0545

    used the cookies() function

--- api_demo/src/app/profile/route.ts
+++ api_demo/src/app/profile/route.ts
@@ -1,11 +1,13 @@
-import { log } from "console";
-import Head from "next/head";
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   console.log(requestHeaders.get("Authorization"));
-  const theme = request.cookies.get("Theme");
-  console.log(theme);
+  // const theme = request.cookies.get("Theme");
+  // console.log(theme); one method of appproach of logging cookies
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerpage", "20"); // method of sending the cookies
+  console.log(cookieStore.get("resultPerpage"));
   return new Response(" THis is profile Api", {
     headers: { "Content-Type": "text/html", "Set-Cookie": "Theme=Dark" },
   });
